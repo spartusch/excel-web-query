f59ccea018102a34f08160e3e087efa89ab06ad3
Stefan Partusch <s.t.partusch+gh@gmail.com>
2019-09-15 14:09:26 +0200

    Rename default factory, add support for web query properties, remove discriminator

--- src/main/java/com/github/spartusch/webquery/WebQueryFactory.java
+++ src/main/java/com/github/spartusch/webquery/WebQueryFactory.java
@@ -4,6 +4,4 @@ public interface WebQueryFactory {
 
     WebQuery create(final String uri, final WebQueryParameter... params);
 
-    WebQuery create(final String uri, final String discriminator, final WebQueryParameter... params);
-
 }
